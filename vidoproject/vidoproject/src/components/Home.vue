4e44ca29d3fff7bbfdc9e0f4eea02e6cb4b75f5b
WooHeeSeop <thaud153@naver.com>
2024-05-19 15:45:26 +0900

    first

---
+++ vidoproject/vidoproject/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ImageSlider/>
+    <br><br><br>
+    <MoiveContent/>
+    <br><br><br>
+    <Animation/>
+  </div>
+</template>
+
+<script setup>
+import ImageSlider from './ImageSlider.vue';
+import Animation from './Animation.vue';
+import MoiveContent from "./MoiveContent.vue";
+</script>
+
+<style scoped>
+/* 필요한 스타일 작성 */
+</style>
